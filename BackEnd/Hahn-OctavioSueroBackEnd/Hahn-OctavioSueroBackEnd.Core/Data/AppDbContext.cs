3f268878a20533d7653fed9fadc41c7242fa6f55
osuero <octavio.suero@gmail.com>
2024-12-12 04:23:56 -0300

    adding db context to manage data with entityFramework

---
+++ BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd.Core/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Hahn_OctavioSueroBackEnd.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hahn_OctavioSueroBackEnd.Core.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd.Core/Entities/Product.cs
+++ BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd.Core/Entities/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace Hahn_OctavioSueroBackEnd.Core.Entities
 {
-    internal class Product
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
