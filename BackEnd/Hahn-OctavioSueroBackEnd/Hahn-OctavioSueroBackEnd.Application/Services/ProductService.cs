df4f40c6cfb0eac5e5f7b9a6b0ed454a2eca8b06
osuero <octavio.suero@gmail.com>
2024-12-12 04:29:14 -0300

    adding service to call repository functionality

---
+++ BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd.Application/Services/ProductService.cs
@@ -0,0 +1,25 @@
+﻿using Hahn_OctavioSueroBackEnd.Core.Entities;
+using Hahn_OctavioSueroBackEnd.Core.Interfaces;
+
+namespace Hahn_OctavioSueroBackEnd.Application.Services
+{
+    public class ProductService
+    {
+        private readonly IRepository<Product> _repository;
+
+        public ProductService(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync() => await _repository.GetAllAsync();
+
+        public async Task<Product> GetProductByIdAsync(int id) => await _repository.GetByIdAsync(id);
+
+        public async Task AddProductAsync(Product product) => await _repository.AddAsync(product);
+
+        public async Task UpdateProductAsync(Product product) => await _repository.UpdateAsync(product);
+
+        public async Task DeleteProductAsync(int id) => await _repository.DeleteAsync(id);
+    }
+}
