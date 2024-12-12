eaa849f12fe25b55666256a37097ec9281a7611c
osuero <octavio.suero@gmail.com>
2024-12-12 04:34:26 -0300

    adding references and using to access to repository, services and context

--- BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd/Program.cs
+++ BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd/Program.cs
@@ -1,20 +1,37 @@
+using Hahn_OctavioSueroBackEnd.Application.Services;
+using Hahn_OctavioSueroBackEnd.Core.Data;
+using Hahn_OctavioSueroBackEnd.Core.Interfaces;
+using Hahn_OctavioSueroBackEnd.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+builder.Services.AddScoped<ProductService>();
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configure Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseExceptionHandler("/error");
+app.UseHsts();
 
 app.UseHttpsRedirection();
 
