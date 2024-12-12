c38a0cef00d17a01d39f80f8c0e5e71914c4c647
osuero <octavio.suero@gmail.com>
2024-12-12 04:45:59 -0300

    adding controller for products

---
+++ BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Hahn_OctavioSueroBackEnd.Application.Services;
+using Hahn_OctavioSueroBackEnd.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hahn_OctavioSueroBackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            await _productService.AddProductAsync(product);
+            return Ok(product);
+        }
+    }
+}
