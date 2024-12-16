bbbb350b54df9b436158efb9f7ec0be93eee8392
osuero <octavio.suero@gmail.com>
2024-12-16 17:05:43 -0300

    adding cors to call from anywhere

--- BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd/Program.cs
+++ BackEnd/Hahn-OctavioSueroBackEnd/Hahn-OctavioSueroBackEnd/Program.cs
@@ -22,9 +22,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
-
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
